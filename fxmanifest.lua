c46fd4f7c3d072fc1a46df86dd4fa56e16212c00
ArdentLeKey <61157993+ArdentLeKey@users.noreply.github.com>
2020-02-17 20:26:11 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Lynx_Events'
+
+version '1.0.0'
+
+server_scripts {
+	'server.lua'
+}
+
+client_scripts {
+	'client.lua'
+}
+
+--dependencies {
+	--'RessourcePourBan.lua'
+--}
