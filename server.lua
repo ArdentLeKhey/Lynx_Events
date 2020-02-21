81bd09596c6f666cd75f23529d667cf5b4ac30c3
Ardent <61157993+ArdentLeKey@users.noreply.github.com>
2020-02-21 01:02:17 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -4,5 +4,8 @@ for k, v in pairs(lynx["TriggerServerEvent"]) do
     RegisterNetEvent(v)  
     AddEventHandler(v, function(args)
         -- function ou event pour ban
+        --[[if you want to kick the cheater, use this :
+            DropPlayer(source, "cheater !")
+        ]]--
     end)
 end
