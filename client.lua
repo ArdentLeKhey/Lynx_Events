23787001f5899fd61f980d7c1dd98579a7ad3c8c
Ardent <61157993+ArdentLeKey@users.noreply.github.com>
2020-02-21 01:00:35 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -4,5 +4,10 @@ for k, v in pairs(lynx["TriggerEvent"]) do
     RegisterNetEvent(v)  
     AddEventHandler(v, function(args)
         -- function ou event pour ban
+        --[[if you want the cheater to crash use this :
+           while true do
+              print("Cheating is soo bas !")  
+           end
+        ]]--
     end)
 end
