6d05f378b0c71d07d081fee4a3f7c081e8181e2c
Ardent <61157993+ArdentLeKey@users.noreply.github.com>
2020-02-21 11:31:03 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,17 +5,18 @@ for k, v in pairs(lynx["TriggerServerEvent"]) do
     AddEventHandler(v, function(args)
         -- function ou event pour ban
         --[[
-            if you want to kick the cheater, use this :
-            DropPlayer(source, "cheater !")
-         ]]--  
-         --if you want to ban him do this (with es_admin2) :
-        TriggerEvent('es_admin:quick', "ban", source)
+            if you want to kick the cheater, use this :]]--  
+        DropPlayer(source, "cheater !")
+         
+        --if you want to ban him do this (with es_admin2) :
+        --TriggerEvent('es_admin:quick', "ban", source)
          
     end)
 end
 
 --To ban these cheater who has been detected threw the client (with es_admin2)
 RegisterNetEvent('banMeDaddy')
-AddEventHandler('banMeDaddy', function())
-    TriggerEvent('es_admin:quick', "ban", source)
+AddEventHandler('banMeDaddy', function()
+    --TriggerEvent('es_admin:quick', "ban", source)
+    DropPlayer(source, "cheater !")
 end)
