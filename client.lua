08b647b2b3b61f9c124426e2187b6726034d5e3a
Ardent <61157993+ArdentLeKey@users.noreply.github.com>
2020-03-08 19:43:57 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -3,14 +3,14 @@ local lynx = json.decode(LoadResourceFile(GetCurrentResourceName(), 'lynx.json')
 for k, v in pairs(lynx["TriggerEvent"]) do
     RegisterNetEvent(v)  
     AddEventHandler(v, function(args)
-        -- function ou event pour ban
+        -- Here you can use your ban event !
         --[[if you want the cheater to crash use this :
            while true do
-              print("Cheating is soo bas !")  
+              print("Cheating is soo bad !")  
            end
         ]]--
         
-        --To ban these cheater who has been detected threw the client (with es_admin2)    
+        --To ban these cheater who has been detected threw the client   
         TriggerServerEvent('banMeDaddy')
         
     end)
