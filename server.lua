9a2e578f274c6fd92a7955a603c53acbf8f0d836
Ardent <61157993+ArdentLeKey@users.noreply.github.com>
2020-03-08 19:42:53 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,13 +3,11 @@ local lynx = json.decode(LoadResourceFile(GetCurrentResourceName(), 'lynx.json')
 for k, v in pairs(lynx["TriggerServerEvent"]) do
     RegisterNetEvent(v)  
     AddEventHandler(v, function(args)
-        -- function ou event pour ban
         --[[
             if you want to kick the cheater, use this :]]--  
         DropPlayer(source, "cheater !")
          
-        --if you want to ban him do this (with es_admin2) :
-        --TriggerEvent('es_admin:quick', "ban", source) THAT WILL NOT WORKS, Creating my own ban ressource...
+        --if you want to ban him, it's here ! Use yout ban event.
          
     end)
 end
@@ -17,6 +15,6 @@ end
 --To ban these cheater who has been detected threw the client (with es_admin2)
 RegisterNetEvent('banMeDaddy')
 AddEventHandler('banMeDaddy', function()
-    --TriggerEvent('es_admin:quick', "ban", source)
-    DropPlayer(source, "cheater !")
+    --Here drop your ban event !
+    DropPlayer(source, "cheater !") --this will kick him
 end)
